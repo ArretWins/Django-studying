a67634046716720e96351b210f231a5eedd025e1
Artsiom <prohorenkoartem111@gmail.com>
2023-07-09 13:13:39 +0300

    crm

--- my_todo/my_app/app/urls.py
+++ my_todo/my_app/app/urls.py
@@ -1,9 +1,12 @@
 from . import views
 from django.urls import path
 
+
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add_todo', views.add_todo, name='add_todo'),
+    path('add/', views.add_todo, name='add_todo'),
     path('update/<int:todo_id>/', views.update, name='update'),
     path('delete/<int:todo_id>/', views.delete, name='delete'),
+    path('full_note/<int:todo_id>/', views.full_note, name='full_note'),
+    path('edit/<int:todo_id>/', views.edit, name='edit'),
 ]
--- my_todo/my_app/app/views.py
+++ my_todo/my_app/app/views.py
@@ -6,7 +6,7 @@ from django.views.decorators.http import require_http_methods
 
 def index(request):
     to_do_list = ToDo.objects.order_by('-id')
-    return render(request, 'app/index.html', {'to_do_list': to_do_list})
+    return render(request, 'app/notes.html', {'to_do_list': to_do_list})
 
 
 @require_http_methods(['POST'])
@@ -29,3 +29,22 @@ def delete(request, todo_id):
     todo = ToDo.objects.get(id = todo_id)
     todo.delete()
     return redirect('index')
+
+def full_note(request, todo_id):
+    to_do_list = ToDo.objects.get(id = todo_id)
+    return render(request, 'app/full_note.html', {'to_do_list': to_do_list})
+
+def edit(request, todo_id):
+    todo = ToDo.objects.get(id = todo_id)
+    
+    
+    if request.method == 'POST':
+        todo.delete()
+        updated_todo = ToDo.objects.get(id=todo_id)
+        updated_todo.title = request.POST['title']
+        updated_todo.note = request.POST['note']
+        updated_todo.save()
+        return redirect('full_note', todo_id=todo_id)
+
+    
+    return render(request, 'app/edit_note.html', {'todo': todo})
